455459e82bb4f2169d1f5c3e1a73d225ea0dd7e3
liuchang <2522343578@qq.com>
2019-04-28 14:34:12 +0800

    first commit

---
+++ src/components/aaa.vue
@@ -0,0 +1,11 @@
+<template>
+    <!--<p id="header">哈哈哈哈哈哈哈</p>-->
+    <p id="aaa">哈哈哈哈哈</p>
+</template>
+
+<style>
+    #aaa{
+        position: absolute;
+        top:100px;
+    }
+</style>
---
+++ src/main.js
@@ -0,0 +1,20 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+// import App from './App'
+import aaa from './components/aaa'
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+// new Vue({
+//   el: '#app',
+//   components: { App },
+//   template: '<App/>'
+// })
+
+new Vue({
+   el: '#aaa',
+  components: { aaa },
+  template: '<aaa/>'
+});
